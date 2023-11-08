31c0e0015b132ddb803923cd5e922622be91719e
marquitos150 <mmvazquez364@gmail.com>
2023-11-07 22:19:51 -0500

    Tested WiFi connection with camera esp32 board

--- esp32-ultrasonic-sensor/Sensor_Transmitter.ino
+++ esp32-ultrasonic-sensor/Sensor_Transmitter.ino
@@ -1,5 +1,6 @@
 #include <NewPing.h>
 #include "WiFi.h"
+#include <esp_wifi.h>
 #include <esp_now.h>
 
 #define MAX_DISTANCE 50
@@ -23,12 +24,17 @@ int distance_waste = 0;
 int distance_recycle = 0;
 
 // Data structure of data to send to responder
+// typedef struct Message
+// {
+//   char message[64];
+//   int trash_data;
+//   int waste_data;
+//   int recycle_data;
+// } Message;
+
 typedef struct Message
 {
-  char message[64];
-  int trash_data;
-  int waste_data;
-  int recycle_data;
+  bool start;
 } Message;
 
 Message data;
@@ -48,13 +54,43 @@ void DataSent(const uint8_t* MAC_ADDRESS, esp_now_send_status_t status)
 }
 
 // MAC Address of responder
-uint8_t broadcastAddress[] = {0xA0, 0xB7, 0x65, 0xFE, 0x72, 0xD4};
+//uint8_t broadcastAddress[] = {0xA0, 0xB7, 0x65, 0xFE, 0x72, 0xD4};
+uint8_t broadcastAddress[] = {0xA0, 0xB7, 0x65, 0xFE, 0xDB, 0x5C};
+
+// Insert your SSID
+constexpr char WIFI_SSID[] = "Angel’s iPhone";
+
+int32_t getWiFiChannel(const char *ssid) {
+  if (int32_t n = WiFi.scanNetworks()) {
+      for (uint8_t i=0; i<n; i++) {
+          if (!strcmp(ssid, WiFi.SSID(i).c_str())) {
+              return WiFi.channel(i);
+          }
+      }
+  }
+  return 0;
+}
 
 void setup() 
 {
   Serial.begin(9600);
 
-  WiFi.mode(WIFI_MODE_STA);
+  // Set device as a Wi-Fi Station and set channel
+  WiFi.mode(WIFI_STA);
+
+  int32_t channel = getWiFiChannel(WIFI_SSID);
+
+  WiFi.printDiag(Serial); // Uncomment to verify channel number before
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+  esp_wifi_set_promiscuous(false);
+  WiFi.printDiag(Serial); // Uncomment to verify channel change after
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
 
   Serial.print("MAC ADDRESS: ");
   Serial.println(WiFi.macAddress());
@@ -86,10 +122,11 @@ void loop()
   distance_waste = sonar2.ping_in(); // Send ping, get distance in inches (0 = outside set distance range)
   distance_recycle = sonar3.ping_in(); // Send ping, get distance in inches (0 = outside set distance range)
 
-  strcpy(data.message, "Reported Distances Listed in Order: {trash, waste, recycle}:");
-  data.trash_data = distance_trash;
-  data.waste_data = distance_waste;
-  data.recycle_data = distance_recycle;
+  // strcpy(data.message, "Reported Distances Listed in Order: {trash, waste, recycle}:");
+  data.start = true;
+  // data.trash_data = distance_trash;
+  // data.waste_data = distance_waste;
+  // data.recycle_data = distance_recycle;
 
   // Send message via EP-NOW
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t*) &data, sizeof(data));
